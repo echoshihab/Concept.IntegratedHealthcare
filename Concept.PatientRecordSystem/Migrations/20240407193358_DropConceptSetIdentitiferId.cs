e89f41629637e51eb5c9e00af8fba71dfa5634b5
Shihab Khan <echoshihab@gmail.com>
2024-04-07 16:01:55 -0400

    drop identifier id from concept set

---
+++ Concept.PatientRecordSystem/Migrations/20240407193358_DropConceptSetIdentitiferId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Concept.PatientRecordSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropConceptSetIdentitiferId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentifierId",
+                table: "ConceptSets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdentifierId",
+                table: "ConceptSets",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
--- Concept.PatientRecordSystem/Persistence/Models/ConceptSet.cs
+++ Concept.PatientRecordSystem/Persistence/Models/ConceptSet.cs
@@ -4,7 +4,6 @@ namespace Concept.PatientRecordSystem.Persistence.Models
 {
     public class ConceptSet : IdentifiedData
     {
-        public Guid IdentifierId { get; set; }
         public string Name { get; set; } = null!;
         public List<Concept> Concepts { get; set; } = [];
         public List<ConceptConceptSet> ConceptConceptSets { get; set; } = [];
