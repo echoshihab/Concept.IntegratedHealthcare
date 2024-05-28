c2dce62619040adf23c9b3e67a2fca3fc8d34b16
Shihab Khan <echoshihab@gmail.com>
2024-05-28 12:09:42 -0400

    make contact point use nullable

--- Concept.PatientRecordSystem/Persistence/Models/PatientTelecom.cs
+++ Concept.PatientRecordSystem/Persistence/Models/PatientTelecom.cs
@@ -3,11 +3,11 @@
     public class PatientTelecom : IdentifiedData
     {
         public Guid ContactSystemConceptId { get; set; }
-        public Guid ContactPointUseConceptId { get; set; }
+        public Guid? ContactPointUseConceptId { get; set; }
         public Guid PatientId { get; set; }
-        public string? Value { get; set; }
+        public string Value { get; set; } = null!;
         public Concept ContactSystemConcept { get; set; } = null!;
-        public Concept ContactPointUseConcept { get; set; } = null!;
+        public Concept? ContactPointUseConcept { get; set; } 
         public Patient Patient { get;set;} = null!;
 
     }
