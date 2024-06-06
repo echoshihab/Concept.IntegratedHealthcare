e84a7ee2fc590fffecbdb55cf9b2a154d2f626cb
Shihab Khan <echoshihab@gmail.com>
2024-06-05 20:04:40 -0400

    add base fhir resource controller

---
+++ Concept.PatientRecordSystem/Controllers/FhirControllerBase.cs
@@ -0,0 +1,28 @@
+﻿using Concept.PatientRecordSystem.Service;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Concept.PatientRecordSystem.Controllers
+{
+    [ApiController]
+    public abstract class FhirControllerBase<TResource> : ControllerBase where TResource: Resource
+    {
+        private IResourceService<TResource> _resourceService;
+        public FhirControllerBase(IResourceService<TResource> resourceService)
+        {
+            _resourceService = resourceService;
+        }
+
+        public virtual async Task<IActionResult> CreateAsync(TResource resource)
+        {
+            if (resource == null)
+            {
+                return BadRequest();
+            }
+
+            var resourcePersisted = await this._resourceService.CreateAsync(resource);
+
+            return this.Ok(resourcePersisted);
+        }
+    }
+}
