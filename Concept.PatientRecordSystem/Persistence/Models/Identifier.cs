fe7b901aa6de70659693093474157207c91eee51
Shihab Khan <echoshihab@gmail.com>
2024-06-10 20:42:18 -0400

    start refactoring persistence models to support person and related properties

--- Concept.PatientRecordSystem/Persistence/Models/Identifier.cs
+++ Concept.PatientRecordSystem/Persistence/Models/Identifier.cs
@@ -2,10 +2,10 @@
 {
     public class Identifier : IdentifiedData
     {
-        public Guid PatientId { get; set; }
+        public Guid PersonId { get; set; }
         public string? System { get; set; }
         public string Value { get; set; } = null!;
-        public Patient Patient { get; set; } = null!;
+        public Person Person { get; set; } = null!;
     }
 }
 
---
+++ Concept.PatientRecordSystem/Persistence/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Concept.PatientRecordSystem.Persistence.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            this.Identifiers = new List<Identifier>();
+            this.Addresses = new List<Address>();
+            this.NameParts = new List<NamePart>();         
+        }
+
+        public ICollection<Identifier> Identifiers { get; set; }
+        public ICollection<NamePart> NameParts { get; set; }
+        public ICollection<Address> Addresses { get; set; }
+        public string PersonType   { get; set; } = null!;
+    }
+}
