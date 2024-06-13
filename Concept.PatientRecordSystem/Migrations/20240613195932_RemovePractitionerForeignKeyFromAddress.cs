676e5943353ead83364d4d1be62939ba925e95b8
Shihab Khan <echoshihab@gmail.com>
2024-06-13 16:00:46 -0400

    remove practitioner foreign key reference from address

---
+++ Concept.PatientRecordSystem/Migrations/20240613195932_RemovePractitionerForeignKeyFromAddress.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Concept.PatientRecordSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePractitionerForeignKeyFromAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Practitioners_PractitionerId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_PractitionerId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "PractitionerId",
+                table: "Addresses");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PractitionerId",
+                table: "Addresses",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_PractitionerId",
+                table: "Addresses",
+                column: "PractitionerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Practitioners_PractitionerId",
+                table: "Addresses",
+                column: "PractitionerId",
+                principalTable: "Practitioners",
+                principalColumn: "Id");
+        }
+    }
+}
--- Concept.PatientRecordSystem/Persistence/Models/Practitioner.cs
+++ Concept.PatientRecordSystem/Persistence/Models/Practitioner.cs
@@ -3,14 +3,12 @@
     public class Practitioner : IdentifiedData
     {
         public Practitioner()
-        {
-            Addresses = [];
+        {            
             Telecoms = [];
         }
 
         public Guid IndividualId { get; set; }
-        public Individual Individual { get; set; } = null!;
-        public List<Address> Addresses { get; set; }        
+        public Individual Individual { get; set; } = null!;        
         public List<PractitionerTelecom> Telecoms { get; set; }        
     }
 }
