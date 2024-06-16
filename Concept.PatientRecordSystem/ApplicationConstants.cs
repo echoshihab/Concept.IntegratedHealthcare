09855247d7d2f520a8bc09f488b4889f99c88e0e
Shihab Khan <echoshihab@gmail.com>
2024-06-16 10:33:18 -0400

    fix incorrect reference to practitioner us core profile

--- Concept.PatientRecordSystem/ApplicationConstants.cs
+++ Concept.PatientRecordSystem/ApplicationConstants.cs
@@ -5,6 +5,6 @@
         public const string PATIENT = "PATIENT";
 
         public const string PatientUSCoreProfile = "http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient";
-        public const string PractitionerUSCoreProfile = "http://hl7.org/fhir/us/core/STU7/StructureDefinition-us-core-practitioner";
+        public const string PractitionerUSCoreProfile = "http://hl7.org/fhir/us/core/StructureDefinition/us-core-practitioner";
     }
 }
