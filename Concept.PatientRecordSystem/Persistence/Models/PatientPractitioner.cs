04d66aa956c9a119072d5b2f91888654bbf05903
Shihab Khan <echoshihab@gmail.com>
2024-06-18 14:38:32 -0400

    add patient practitioner model

---
+++ Concept.PatientRecordSystem/Persistence/Models/PatientPractitioner.cs
@@ -0,0 +1,11 @@
+﻿namespace Concept.PatientRecordSystem.Persistence.Models
+{
+    public class PatientPractitioner : IdentifiedData
+    {
+        public Guid PatientId { get; set; }
+        public Patient Patient { get;set; } = null!;
+        public Guid PractitionerReferenceId { get; set; }       
+        public Guid PractitionerTypeConceptId { get; set; }
+        public Concept PractitionerTypeConcept { get; set; } = null!;
+    }
+}
