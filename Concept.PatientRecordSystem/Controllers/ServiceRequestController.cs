fcc8c0802e0711eb7c0f56ed3a1c34c9d5902af2
Shihab Khan <echoshihab@gmail.com>
2024-09-10 21:16:12 -0400

    add controller and service for service request, and test service request example json

---
+++ Concept.PatientRecordSystem/Controllers/ServiceRequestController.cs
@@ -0,0 +1,19 @@
+﻿using Concept.PatientRecordSystem.Service;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Concept.PatientRecordSystem.Controllers
+{
+    public class ServiceRequestController : FhirControllerBase<ServiceRequest>
+    {
+        public ServiceRequestController(IResourceService<ServiceRequest> serviceRequestResourceService) : base(serviceRequestResourceService)
+        {
+            
+        }
+
+        public override Task<IActionResult> CreateAsync(ServiceRequest resource)
+        {
+            return base.CreateAsync(resource);
+        }
+    }
+}
---
+++ Concept.PatientRecordSystem/Service/ServiceRequestService.cs
@@ -0,0 +1,18 @@
+﻿using Concept.PatientRecordSystem.Persistence;
+
+namespace Concept.PatientRecordSystem.Service
+{
+    public class ServiceRequestService : ResourcePersistenceServiceBase<Hl7.Fhir.Model.ServiceRequest>
+    {
+        public ServiceRequestService(ApplicationDbContext context): base(context)
+        {
+            
+        }
+
+        public override Task<Hl7.Fhir.Model.ServiceRequest> CreateAsync(Hl7.Fhir.Model.ServiceRequest fhirResource)
+        {
+            //TODO : add service request logic
+            return base.CreateAsync(fhirResource);
+        }
+    }
+}
