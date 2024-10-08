a02aa72fa281606c62fe00e9d90689a7ea699a38
Shihab Khan <echoshihab@gmail.com>
2024-10-08 16:00:34 -0400

    add ServiceRequest.RequesterId foreign key update related migrations

---
+++ Concept.PatientRecordSystem/Migrations/20241008195334_configureRequesterServiceRequestForeignKey.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Concept.PatientRecordSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class configureRequesterServiceRequestForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRequests_PatientPractitioners_RequesterId",
+                table: "ServiceRequests");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_PatientPractitioners_PractitionerReferenceId",
+                table: "PatientPractitioners",
+                column: "PractitionerReferenceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRequests_PatientPractitioners_RequesterId",
+                table: "ServiceRequests",
+                column: "RequesterId",
+                principalTable: "PatientPractitioners",
+                principalColumn: "PractitionerReferenceId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRequests_PatientPractitioners_RequesterId",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_PatientPractitioners_PractitionerReferenceId",
+                table: "PatientPractitioners");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRequests_PatientPractitioners_RequesterId",
+                table: "ServiceRequests",
+                column: "RequesterId",
+                principalTable: "PatientPractitioners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
