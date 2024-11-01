385a77fa1a10299d70ed272fbf940cff345f465c
Shihab Khan <echoshihab@gmail.com>
2024-11-01 16:59:23 -0400

    inject mapping and persistence service to handle patient dto to patient mapping and persistence

--- Concept.PatientRecordSystem/Service/Domain/DomainResourceServiceBase.cs
+++ Concept.PatientRecordSystem/Service/Domain/DomainResourceServiceBase.cs
@@ -1,19 +1,22 @@
 ﻿using Concept.PatientRecordSystem.DTOs;
 using Concept.PatientRecordSystem.Persistence.Models;
+using Concept.PatientRecordSystem.Persistence.Service;
 using Hl7.Fhir.Model;
 
 namespace Concept.PatientRecordSystem.Service.Domain
 {
     public abstract class DomainResourceServiceBase<TDomain, TPersistence>: IDomainService<TDomain, TPersistence> where TDomain : IdentifiableData where TPersistence: IdentifiedData
     {
-        public readonly IMappingService<TDomain, TPersistence> _mappingService;
+        protected readonly IMappingService<TDomain, TPersistence> _mappingService;
+        protected readonly IPersistenceService<TPersistence> _persistenceService;
 
-        protected DomainResourceServiceBase(IMappingService<TDomain, TPersistence> mappingService, IResourceService<T>)
+        protected DomainResourceServiceBase(IMappingService<TDomain, TPersistence> mappingService, IPersistenceService<TPersistence> persistenceService)
         {
             _mappingService = mappingService;
+            _persistenceService = persistenceService;
         }
 
-        public Task<TDomain> CreateAsync(TDomain resource)
+        public virtual Task<TDomain> CreateAsync(TDomain resource)
         {
             throw new NotImplementedException();
         }
--- Concept.PatientRecordSystem/Service/Domain/PatientDomainService.cs
+++ Concept.PatientRecordSystem/Service/Domain/PatientDomainService.cs
@@ -1,21 +1,22 @@
 ﻿using Concept.PatientRecordSystem.DTOs;
 using Concept.PatientRecordSystem.Persistence.Models;
+using Concept.PatientRecordSystem.Persistence.Service;
 
 namespace Concept.PatientRecordSystem.Service.Domain
 {
     public class PatientDomainService : DomainResourceServiceBase<PatientDto, Patient>
     {
         
-        public PatientDomainService(IMappingService<PatientDto, Patient> mappingService) : base(mappingService)
-        {
+        public PatientDomainService(IMappingService<PatientDto, Patient> mappingService, IPersistenceService<Patient> persistenceService) : base(mappingService, persistenceService)
+        { 
             
         }
 
-        public override Task<Patient> CreateAsync(PatientDto domainResource)
+        public override async Task<PatientDto> CreateAsync(PatientDto domainResource)
         {
             var patientDb = base._mappingService.MapToDatabaseModel(domainResource);
 
-
+            return base._mappingService.MapToDomainModel(await this._persistenceService.CreateAsync(patientDb));        
         }
     }
 }
