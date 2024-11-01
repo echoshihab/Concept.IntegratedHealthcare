6cdea449f8bc0acd37d3860ce29bd394e7e5e9f5
Shihab Khan <echoshihab@gmail.com>
2024-11-01 16:58:24 -0400

    add patient persistence service with create

---
+++ Concept.PatientRecordSystem/Persistence/Service/PatientPersistenceService.cs
@@ -0,0 +1,20 @@
+﻿using Concept.PatientRecordSystem.Persistence.Models;
+
+namespace Concept.PatientRecordSystem.Persistence.Service
+{
+    public class PatientPersistenceService : PersistenceServiceBase<Patient>
+    {
+        public PatientPersistenceService(ApplicationDbContext context): base(context)
+        {
+            
+        }
+
+        public override Task<Patient> CreateAsync(Patient resource)
+        {
+
+            return base.CreateAsync(resource);
+        }
+    }
+
+    
+}
