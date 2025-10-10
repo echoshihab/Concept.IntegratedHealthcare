f99fab488cfd3a593335f6391a007b7751b3c7c9
Shihab Khan <echoshihab@gmail.com>
2025-10-09 21:31:43 -0400

    implement patient queue service

---
+++ Concept.PatientRecordSystem/Service/Queue/PatientQueueService.cs
@@ -0,0 +1,13 @@
+﻿using Hl7.Fhir.Model;
+using MassTransit;
+
+namespace Proto.PatientRecordSystem.Service.Queue
+{
+    public class PatientQueueService : ResourceQueueServiceBase<Patient>
+    {
+        public PatientQueueService(IPublishEndpoint publishEndpoint) : base(publishEndpoint)
+        {
+            
+        }
+    }
+}
