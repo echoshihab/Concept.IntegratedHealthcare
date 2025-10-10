abe3a47cf1e98bcb1f79a6a909f714e2c8124e50
Shihab Khan <echoshihab@gmail.com>
2025-10-09 21:32:17 -0400

    add scaffolding for integration service

---
+++ Concept.PatientRecordSystem/Service/Integration/IntegrationServiceBase.cs
@@ -0,0 +1,25 @@
+﻿using Proto.PatientRecordSystem.Service.Integration.Interfaces;
+using Proto.PatientRecordSystem.Service.Mapping.Interfaces;
+using Proto.PatientRecordSystem.Service.Queue.Interfaces;
+
+namespace Proto.PatientRecordSystem.Service.Integration
+{
+    public abstract class IntegrationServiceBase<TDbEntity, TFhirResource> : IIntegrationService<TDbEntity>
+    {
+        private readonly IFhirMappingService<TDbEntity, TFhirResource> _fhirMappingService;
+        private readonly IResourceQueueService<TFhirResource> _resourceQueueService;
+
+        protected IntegrationServiceBase(IFhirMappingService<TDbEntity, TFhirResource> fhirMappingService, IResourceQueueService<TFhirResource> resourceQueueService)
+        {
+            _fhirMappingService = fhirMappingService;
+            _resourceQueueService = resourceQueueService;
+        }
+
+        public async virtual void Send(TDbEntity dbEntity)
+        {
+            var fhirResource = await _fhirMappingService.MapToFhirResourceAsync(dbEntity);
+
+            _resourceQueueService.publish(fhirResource);
+        }
+    }
+}
---
+++ Concept.PatientRecordSystem/Service/Integration/Interfaces/IIntegrationService.cs
@@ -0,0 +1,7 @@
+﻿namespace Proto.PatientRecordSystem.Service.Integration.Interfaces
+{
+    public interface IIntegrationService<TDbEntity>
+    {
+        void Send(TDbEntity dbEntity);
+    }
+}
