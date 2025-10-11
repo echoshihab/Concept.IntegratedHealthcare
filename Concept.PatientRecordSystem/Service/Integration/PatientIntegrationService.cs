c76bc76429a3754572d7448fb97a183858972f3f
Shihab Khan <echoshihab@gmail.com>
2025-10-10 20:53:29 -0400

    scaffold patient ingeration service

---
+++ Concept.PatientRecordSystem/Service/Integration/PatientIntegrationService.cs
@@ -0,0 +1,13 @@
+﻿using Proto.PatientRecordSystem.Persistence.Models;
+using Proto.PatientRecordSystem.Service.Mapping.Interfaces;
+using Proto.PatientRecordSystem.Service.Queue.Interfaces;
+
+namespace Proto.PatientRecordSystem.Service.Integration
+{
+    public class PatientIntegrationService : IntegrationServiceBase<Patient, Hl7.Fhir.Model.Patient>
+    {
+        public PatientIntegrationService(IFhirMappingService<Patient, Hl7.Fhir.Model.Patient> fhirMappingService, IResourceQueueService<Hl7.Fhir.Model.Patient> resourceQueueService) : base(fhirMappingService, resourceQueueService)
+        {
+        }
+    }
+}
