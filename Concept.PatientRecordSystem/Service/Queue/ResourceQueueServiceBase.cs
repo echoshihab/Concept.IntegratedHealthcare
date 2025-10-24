198ab6ae81042ebdb980d237b674701bccb840f1
Shihab Khan <echoshihab@gmail.com>
2025-10-23 20:46:29 -0400

    switch out publishEndpoint for sendEndpointProvider from masstransit

--- Concept.PatientRecordSystem/Service/Queue/ResourceQueueServiceBase.cs
+++ Concept.PatientRecordSystem/Service/Queue/ResourceQueueServiceBase.cs
@@ -1,26 +1,30 @@
 ﻿using Hl7.Fhir.Model;
 using MassTransit;
+using MassTransit.Transports;
 using Proto.PatientRecordSystem.Service.Queue.Interfaces;
 using Task = System.Threading.Tasks.Task;
 
 namespace Proto.PatientRecordSystem.Service.Queue
 {
     public abstract class ResourceQueueServiceBase<TResource> : IResourceQueueService<TResource> where TResource: Resource
-    {
-        private readonly IPublishEndpoint _publishEndpoint;
+    {        
         protected string? routingKey = null;
+        private readonly ISendEndpointProvider _sendEndpointProvider;
 
-        protected ResourceQueueServiceBase(IPublishEndpoint publishEndpoint)
+        protected ResourceQueueServiceBase(ISendEndpointProvider _sendEndpointProvider)
         {
-            _publishEndpoint = publishEndpoint;
+            this._sendEndpointProvider = _sendEndpointProvider;
         }
         public async virtual Task PublishAsync(TResource resource)
         {
             if (this.routingKey == null) throw new ArgumentNullException(nameof(routingKey));
 
-            await _publishEndpoint.Publish(resource, context => { 
-                    context.SetRoutingKey(this.routingKey);
-                }); 
+            var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("exchange:Inh.FhirResource"));
+
+            await sendEndpoint.Send(resource, context =>
+            {
+                context.SetRoutingKey(this.routingKey);
+            });
         }
     }
 }
